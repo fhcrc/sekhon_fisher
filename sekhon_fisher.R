8dd866175fa8651471f9cabfcce844c296d24c81
Christopher Small <csmall@fhcrc.org>
2012-10-11 16:22:37 -0700

    Implimented integral solution as R function

--- sekhon_fisher.R
+++ sekhon_fisher.R
@@ -1,3 +1,11 @@
 #!/usr/bin/env Rscript
 
 suppressMessages(library(stats))
+
+sekhon.test <- function(a, b, c, d, prob.diff=0.0) {
+    f <- function(t) {
+        (1 - pbeta(t + prob.diff, a+1, b+1))*dbeta(t, c+1, d+1)
+    }
+    integrate(f, lower = 0.0, upper=1.0)
+}
+
